607ba52e4b3fce66755081c3c871c8bcfb9272de
geoff <codaern@gmail.com>
2018-08-27 14:51:59 +0200

    création page index, connection et inscription.

---
+++ vue/inscription.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form class="center" action="index.html" method="post">
+      <div class="">
+        <label for="nom">votre nom de famille</label>
+        <input type="text" name="nom" value="" required>
+      </div>
+      <div class="">
+        <label for="prenom">votre prénom</label>
+        <input type="text" name="prenom" value="" required>
+      </div>
+      <div class="">
+        <label for="pass">un password</label>
+        <input type="password" name="password" value="" required>
+      </div>
+      <div class="">
+        <label for="checkpass">votre password encore une fois ;)</label>
+        <input type="password" name="password" value="" required>
+      </div>
+    </form>
+  </body>
+</html>
