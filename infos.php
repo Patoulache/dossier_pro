43dbd2930e69406069f00bcfbd9443a7b29b5fea
geoff <codaern@gmail.com>
2018-08-27 11:05:04 +0200

    fix nom

--- infos.php
+++ infos.php
@@ -14,10 +14,10 @@ class Infos extends Bdd
   {
     $this->values = $arr;
   }
-// function connection base dbConnect()
+// function connection base getBdd()
   public function setIdNom(){
 
-    $sql = $this->dbConnect->prepare('INSERT INTO dossiers (ID_P, Prenom, Nom_N_proprio, Nom_U_Proprio, Adresse) VALUES (:id, :prenom, :nom_n, :nom_u, :addr)');
+    $sql = $this->getBdd()->prepare('INSERT INTO dossiers (ID_P, Prenom, Nom_N_proprio, Nom_U_Proprio, Adresse) VALUES (:id, :prenom, :nom_n, :nom_u, :addr)');
     $sql->bindparam(':id', $this->values['id']);
     $sql->bindparam(':prenom', $this->values['prenom']);
     $sql->bindparam(':nom_n', $this->values['nomnaissance']);
@@ -27,7 +27,7 @@ class Infos extends Bdd
   }
 
   public function getIdNom(){
-    $sql = $this->dbConnect->prepare('SELECT id FROM table WHERE nomnaissance = :nomnaissance AND prenom = :prenom');
+    $sql = $this->getBdd()->prepare('SELECT id FROM table WHERE nomnaissance = :nomnaissance AND prenom = :prenom');
     $sql->bindparam(":nomnaissance", $this->values['nom']);
     $sql->bindparam(":prenom", $this->values['prenom']);
     $sql->execute();
@@ -40,7 +40,7 @@ class Infos extends Bdd
     //trop d'entrées dans l'objet $this->values pour les entrées dans la table, alors ça ma gavé, je stock tout ici.
     //
     $tout = json_encode($this->values);
-     $sql = $this->dbConnect->prepare('INSERT INTO dossier (Nom_F) VALUE (:nom_f) WHERE id = (SELECT id FROM dossier WHERE nomusage = :nomusage)');
+     $sql = $this->getBdd()->prepare('INSERT INTO dossier (Nom_F) VALUE (:nom_f) WHERE id = (SELECT id FROM dossier WHERE nomusage = :nomusage)');
      $sql->bindparam(':nom_f', $tout);
      $sql->bindparam(':nomusage', $this->value["nomusage"]);
      $sql->execute();
