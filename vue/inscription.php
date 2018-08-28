2e6ff1112314bcaa07ed49e92ee33c985770d433
geoff <codaern@gmail.com>
2018-08-28 10:55:49 +0200

    adjust inscription

--- vue/inscription.php
+++ vue/inscription.php
@@ -23,14 +23,17 @@
             <td><label for="prenom">votre prénom</label></td>
             <td><input type="text" name="prenom" value="" required></td>
           </tr>
-
+          <tr>
+            <td>adresse mail</td>
+            <td> <input type="email" name="email" value=""></td>
+          </tr>
           <tr>
             <td><label for="pass">un password</label></td>
-            <td><input type="password" name="password" value="" required></td>
+            <td><input type="password" id="password1" value="" required></td>
           </tr>
           <tr>
             <td><label for="checkpass">votre password encore une fois ;)</label></td>
-            <td><input type="password" name="password" value="" required></td>
+            <td><input type="password" id="password2" name="password" value="" required></td>
           </tr>
           <tr>
             <td></td>
