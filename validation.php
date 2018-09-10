a4f659d417e28de944bdef58e9e67f0b0633029d
Unknown <luca.rosso30@gmail.com>
2018-09-10 16:53:49 +0200

    modif token en cours

--- validation.php
+++ validation.php
@@ -17,8 +17,8 @@ class validationModel extends Bdd {
         $cle = $_GET['cle'];
 
         // Récupération de la clé correspondant au $nom_usage dans la base de données
-        $sql = $this->getBdd()->prepare("SELECT cle,actif FROM table1 WHERE nom_usage = :login");
-        if($sql->execute(array(":login" => $login)) && $row = $sql->fetch())
+        $sql = $this->getBdd()->prepare("SELECT cle,actif FROM table1 WHERE nom_usage = :nom");
+        if($sql->execute(array(":nom" => $login)) && $row = $sql->fetch())
           {
             $clebdd = $row['cle'];	// Récupération de la clé
             $actif = $row['actif']; // $actif contiendra alors 0 ou 1
@@ -36,15 +36,19 @@ class validationModel extends Bdd {
                {
                   // Si elles correspondent on active le compte !	
                   echo "Votre compte a bien été activé !";
+
+                  // Création token :
+                  $token = md5(microtime(TRUE)*100000);
             
                   // La requête qui va passer notre champ actif de 0 à 1
-                  $stmt = $this->getBdd()->prepare("UPDATE table1 SET actif = 1 WHERE nom_usage = ");
+                  $stmt = $this->getBdd()->prepare("UPDATE table1 SET actif = 1,token = :token WHERE nom_usage = :nom");
                   $stmt->bindParam(':nom', $login);
+                  $stmt->bindParam(':token', $token);
                   $stmt->execute();
                }
              else // Si les deux clés sont différentes on provoque une erreur...
                {
-                  echo "Erreur ! Votre compte ne peut être activé...";
+                  echo "Erreur ! Votre compte ne peut être activé et va s'autodétruire dans 10 secondes...";
                }
           }
       
