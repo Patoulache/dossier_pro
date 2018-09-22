9e267648dbb52c7a70a0d2b2e77c1ccfe1990233
jiep <geoff.mirgon@gmail.com>
2018-09-22 11:47:10 +0200

    to try !!!

--- js/connection.js
+++ js/connection.js
@@ -4,6 +4,8 @@ CONNECTION = {
     document.addEventListener('keyup', CONNECTION.checkEntry);
   },
 
+
+//TODO: try if if document.getElementById('email').value > 0 && document.getElementById('pass').value.value > 0
   checkEntry: function(){
     if (document.getElementById('email').value != "" && document.getElementById('pass').value != "") {
       document.getElementById('btnconnection').disabled = false;
