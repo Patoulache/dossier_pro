a3d6d098f3d7bb890b6b47f492360a2d774b6592
Droopy <luca.rosso30@gmail.com>
2018-09-24 16:56:24 +0200

    supp form en cours

--- js/implexemple.js
+++ js/implexemple.js
@@ -15,42 +15,30 @@ FORMAT = {
     
     ciblage : function(e) {
         var cible = e.target.getAttribute("data-exemple"); // récupere nom de l'attribut
+        if (e.target.value === "") {
+            var papa = document.querySelector("#".concat(cible));
+            FORMAT.div.removeChild(papa);
+            console.log("perdu");
+        };
         // check si formulaires existent deja, sinon elle les cree
         if (document.querySelector("input[data-example="+"\""+cible+"\"]") === null) {
             FORMAT.exemple(cible);
             var champs = document.querySelector("input[data-example="+"\""+cible+"\"]");
-            // cible le bon input dans lequel on injecte la valeur rentree dans le sommaire
-            switch(true) {
-                case (cible == "act1ex1"):
-                champs.value = e.target.value; // actualise la valeur du nom de l'exemple.
-                break;
-                case (cible == "act1ex2"):
-                champs.value = e.target.value;
-                break;
-                case (cible == "act1ex3"):
-                champs.value = e.target.value;
-                break;
-                case (cible == "act2ex1"):
-                champs.value = e.target.value;
-                break;
-                case (cible == "act2ex2"):
-                champs.value = e.target.value;
-                break;
-                case (cible == "act2ex3"):
-                champs.value = e.target.value;
-                break;
-                
-            };
+            // cible le bon input dans lequel on injecte la valeur rentree dans le sommaire 
+            champs.value = e.target.value; // actualise la valeur du nom de l'exemple
+
         } else { // actualisation simple du nom de l'exemple si formulaire deja present
             var champs = document.querySelector("input[data-example="+"\""+cible+"\"]");
             champs.value = e.target.value;
-       
+
         };
     },
     // copie le formulaire pour les exemples avec les bon numeros
     exemple : function(cible) {
         var regex = /\d/g;
         var rep = cible.match(regex);
+        var newDiv = document.createElement('div');
+        newDiv.id = cible;
         var cop = FORMAT.div.children[0].cloneNode(true);
         var cop1 = FORMAT.div.children[1].cloneNode(true);
         var cop2 = FORMAT.div.children[2].cloneNode(true);
@@ -59,9 +47,11 @@ FORMAT = {
         cop3[0].innerHTML = cop3[0].innerHTML.replace("1", rep[0]);
         cop4[1].setAttribute("data-example", cible);
         cop4[0].innerHTML = cop4[0].innerHTML.replace("1", rep[1]);
-        FORMAT.div.append(cop, cop1, cop2);
+        FORMAT.div.appendChild(newDiv);
+        newDiv.append(cop, cop1, cop2);
 
     },
+    // test suppression form si erreur
 
 }
 
